33f35c54de26caeed1284325eac9262f43fed13f
Davi Ferreira <davi.ferreira@corp.globo.com>
2014-06-26 13:40:18 -0300

    Adds jshint

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@
 
 var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
-    prefix = require('gulp-autoprefixer');
+    prefix = require('gulp-autoprefixer'),
+    jshint = require('gulp-jshint');
 
 gulp.task('sass', function () {
     gulp.src('./src/sass/**/*.scss')
@@ -16,10 +17,21 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('./demo/css'));
 });
 
+gulp.task('lint', function() {
+    return gulp.src('./src/js/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
+});
+
 gulp.task('watch', function () {
     var watcherCSS = gulp.watch(['./src/sass/**/*.scss',
-                                 './demo/sass/**/*.scss'], ['sass']);
+                                 './demo/sass/**/*.scss'], ['sass']),
+        watcherJS = gulp.watch('./src/js/**/*.js', ['js']);
     watcherCSS.on('changed', function (event) {
+        console.log('File ' + event.path + ' was ' + event.type +
+                    ', running tasks...');
+    });
+    watcherJS.on('changed', function (event) {
         console.log('File ' + event.path + ' was ' + event.type +
                     ', running tasks...');
     });
